daa8f99c53f688603042c7b106409e84392ddd93
Luka Prelevic <lukaprelevic@gmail.com>
2017-11-09 17:19:39 +0000

    finished the backend of cocktail mister

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,42 @@
 class DosesController < ApplicationController
+  # def index
+  #   @cocktails = Cocktail.all
+  # end
+
+  # def show
+  #   @cocktail = Cocktail.find(params[:id])
+  # end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail), notice: 'Dose was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    @cocktail = Cocktail.find(@dose.cocktail)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Tulip")
+Cocktail.create(name: "Blah")
+Cocktail.create(name: "Disco")
